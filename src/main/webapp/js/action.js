a4335daa40ef09e0605b79b0d6e2a5d73c82860c
AFrolov <andrey.v.frolov@gmail.com>
2017-04-01 23:44:19 +0500

    dev

--- src/main/webapp/js/action.js
+++ src/main/webapp/js/action.js
@@ -22,8 +22,56 @@ $(document).ready(function () {
                         emojiArea.append('<br>');
                     }
                 }
+                $('img').click(clickEmoji);
             }
         })
+    }
+
+    function clickEmoji() {
+
+        var img_smiley =$(this).clone().css("width","18px").wrap("<div />").parent().html();
+
+        console.log('>>> emoji clicked ' + $(this).attr('src'));
+        newMessageArea.focus();
+        pasteHtmlAtCaret(img_smiley); //adding html at cursorlocation BY - Tim Down
+        return false;
+    }
+
+    function pasteHtmlAtCaret( html ) {
+        var sel, range;
+        if ( window.getSelection ) {
+            // IE9 and non-IE
+            sel = window.getSelection();
+            if ( sel.getRangeAt && sel.rangeCount ) {
+                range = sel.getRangeAt( 0 );
+                range.deleteContents();
+
+
+                // Range.createContextualFragment() would be useful here but is
+                // only relatively recently standardized and is not supported in
+                // some browsers (IE9, for one)
+                var el = document.createElement( "div" );
+                el.innerHTML = html;
+                var frag = document.createDocumentFragment(),
+                    node, lastNode;
+                while ( ( node = el.firstChild ) ) {
+                    lastNode = frag.appendChild( node );
+                }
+                range.insertNode( frag );
+
+                // Preserve the selection
+                if ( lastNode ) {
+                    range = range.cloneRange();
+                    range.setStartAfter( lastNode );
+                    range.collapse( true );
+                    sel.removeAllRanges();
+                    sel.addRange( range );
+                }
+            }
+        } else if ( document.selection && document.selection.type != "Control" ) {
+            // IE < 9
+            document.selection.createRange().pasteHTML( html );
+        }
     }
 
     function addNewMessage() {
